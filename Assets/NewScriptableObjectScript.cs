d126ebada8df77dc98ce96ed63d5ec6797d86ef6
sujansooriyakumar <43353100+sujansooriyakumar@users.noreply.github.com>
2026-02-24 13:57:59 -0700

    folder cleanup

--- Assets/NewScriptableObjectScript.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "sample", menuName = "Scriptable Objects/sample")]
-public class NewScriptableObjectScript : ScriptableObject
-{
-    public string characterName;
-    public int maximumHP;
-    public int strength;
-}
